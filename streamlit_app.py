6c831e1bf1764195c4bd2ceef1d7ca78b95228c4
Anish De <78080586+cinemax200245@users.noreply.github.com>
2023-08-23 21:47:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,15 @@
-import streamlit
-streamlit.title("ABCD")
+import streamlit as st
+import random
+
+def main():
+    st.title("Random Number Generator")
+    
+    # Generate a random number
+    random_number = random.randint(1, 100)
+    
+    st.write(f"Generated Random Number: {random_number}")
+    
+    st.write("Welcome to this simple Streamlit app. It generates a random number and displays it.")
+    
+if __name__ == "__main__":
+    main()
