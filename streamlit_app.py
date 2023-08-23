bf890a57f4186e77372fadc291cc9d628c22f9c1
Anish De <78080586+cinemax200245@users.noreply.github.com>
2023-08-23 21:49:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,4 @@
 import streamlit as st
-import random
-
-def main():
-    st.title("Random Number Generator")
-    
-    # Generate a random number
-    random_number = random.randint(1, 100)
-    
-    st.write(f"Generated Random Number: {random_number}")
-    
-    st.write("Welcome to this simple Streamlit app. It generates a random number and displays it.")
-    
-if __name__ == "__main__":
-    main()
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
